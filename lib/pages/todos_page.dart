a67f493ed396272e4edd6f3ff66251db17ee23c2
lightlitebug <light.lite.bug@gmail.com>
2024-03-01 21:15:47 +0900

    CreateTodo widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -24,6 +24,7 @@ class _TodosPageState extends State<TodosPage> {
             child: Column(
               children: [
                 TodoHeader(),
+                CreateTodo(),
               ],
             ),
           ),
@@ -53,6 +54,37 @@ class TodoHeader extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({super.key});
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final newTodoController = TextEditingController();
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: const InputDecoration(labelText: 'What to do?'),
+      onSubmitted: (String? todoDesc) {
+        if (todoDesc != null && todoDesc.trim().isNotEmpty) {
+          context.read<TodoList>().addTodo(todoDesc);
+          newTodoController.clear();
+        }
+      },
     );
   }
 }
